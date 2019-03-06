3ca29e0b4890c1841415a83ede274dd3d4bdf693
Davy de Matos <davymatos01@gmail.com>
2019-03-05 22:28:36 -0300

    Criação de migrations e modificação inicial do model

---
+++ app/login.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class login extends Model
+{
+    protected $table = 'login';
+    protected $fillable = ['nome', ...];
+    public $timestamps = false;
+}
--- database/migrations/2019_03_06_010514_login.php
+++ database/migrations/2019_03_06_010514_login.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Login extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('login', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('usuario');
+            $table->increments('id');
+            $table->string('nome');
             $table->string('senha');
-            $table->timestamps();
         });
     }
 
